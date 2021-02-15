d303bb3d0ee9a4b0361f75fb8ba45c768d7758c7
Arturo Fierro <54043392+ArturoFierro98@users.noreply.github.com>
2021-02-14 21:23:07 -0700

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -11,11 +11,20 @@
     <title>Bienvenido</title>
   </head>
   <body>
-    <h1>Bienvenido a mi pagina </h1>
-    <p>Mi nombre es Javier Arturo Fierro Vargas tengo 22 años de edad.
-      actualmente estudio ingenieria en sistemas computacionales en la Universidad autonoma de Ciudad Juarez.
-      siempre he tenido un gran gusto por la tecnologia.  
-    </p>
+
+<?php
+    $clave = hash('sha256', 'esta es una clave secreta', true);
+    $entrada = "Encontrémonos a las 9 en punto en el escondite.";
+
+    $td = mcrypt_module_open('rijndael-128', '', 'cbc', '');
+    $iv = mcrypt_create_iv(mcrypt_enc_get_iv_size($td), MCRYPT_DEV_URANDOM);
+    mcrypt_generic_init($td, $clave, $iv);
+    $datos_encriptados = mcrypt_generic($td, $entrada);
+    mcrypt_generic_deinit($td);
+    mcrypt_module_close($td);
+?>
+
+
 
     <!-- Optional JavaScript; choose one of the two! -->
 
